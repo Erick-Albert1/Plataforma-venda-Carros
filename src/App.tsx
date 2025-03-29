fbd3ba86c296481b091af337c5617aa3470c6ef7
Erick-Albert1 <erick03plus@gmail.com>
2025-03-28 22:07:20 -0300

    Fazendo a proteção das rotas, para que usuarios não logados não possam acessar a pagina /dashboard (exempo)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { CarDetail } from './pages/car'
 
 
 import { Layout } from './components/layout'
+import { Private } from './routes/Private'
 
 const router = createBrowserRouter([
   {
@@ -24,11 +25,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/dashboard",
-        element: <Dashboard/>
+        element: <Private><Dashboard/></Private>
       },
       {
         path: "/dashboard/new",
-        element: <New/>
+        element: <Private><New/></Private>
       }
     ]
   },
