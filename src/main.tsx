5bc3292d7aae0abd772d4e3c03dae0243f4c6d00
Erick-Albert1 <erick03plus@gmail.com>
2025-03-28 21:52:10 -0300

    Trabalhando com Context API, contexto de autenticaçao

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import './index.css'
 import { router } from './App.tsx'
 
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './context/authContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={router}/>
+    </AuthProvider>
   </StrictMode>,
 )
