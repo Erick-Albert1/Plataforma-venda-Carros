50f1de68a97998668687d5cd139ba45d727be4f0
Erick-Albert1 <erick03plus@gmail.com>
2025-03-29 12:20:54 -0300

    Cadastrando dacos do carro no FireStore DataBase, para cada usuário

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -10,12 +10,13 @@ import { ChangeEvent, useState, useContext } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 import { v4 as uuidv4} from 'uuid'
 
-import { storage } from "../../../services/firebaseConnection";
+import { storage, db } from "../../../services/firebaseConnection";
 import { ref,
   uploadBytes,
   getDownloadURL,
   deleteObject
  } from "firebase/storage";
+ import { addDoc, collection } from "firebase/firestore";
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatorio"),
   model: z.string().nonempty("O modelo é obrigatorio"),
@@ -85,7 +86,43 @@ if(e.target.files && e.target.files[0]){
   }
 
   function onSubmit(data: FormData){
-console.log(data);
+    if(carImages.length === 0){
+      alert("Envie alguma imagem deste carro!")
+      return;
+    }
+
+    const carListImages = carImages.map( car => {
+      return{
+        uid: car.uid,
+        name: car.name,
+        url: car.url
+      }
+    })
+    
+    addDoc(collection(db, "cars"),{
+      name: data.name,
+      model:data.model,
+      whatsapp: data.whatsapp,
+      city: data.city,
+      year: data.year,
+      km: data.km,
+      price: data.price,
+      description: data.description,
+      created: new Date(),
+      owner: user?.name,
+      uid: user?.uid,
+      images: carListImages,
+    })
+    .then(()=>{
+      reset();
+      setCarimages([]);
+      console.log("Cadastrado com sucesso!")
+    })
+    .catch((error)=>{
+      console.log(error)
+      console.log("Erro ao cadastar no banco")
+    })
+
   }
   async function handleDeleteImage(item: ImageItemProps){
     const imagePath = `images/${item.uid}/${item.name}`;
